7ccb094f19157ab849d5c4540291b581f28cd4d8
Orest Onatsko <orestonatsko@gmail.com>
2019-11-01 23:17:10 +0200

    created new project

---
+++ src/main/java/com/orest/goodstore/GoodstoreApplication.java
@@ -0,0 +1,13 @@
+package com.orest.goodstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orest/goodstore/GoodstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orest.goodstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
